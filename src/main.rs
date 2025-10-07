e6c8b707acb66dbda4700d96d40f2ff4104cd4db
yogevh022 <yogevh022@gmail.com>
2025-10-07 19:54:33 +0300

    first working fast slab implementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::hint::black_box;
-use slabmap::SlabMap;
+use ::slab::Slab;
+use slabmap::{BitmapSlab, SlabMap};
 mod slab;
 #[macro_export]
 macro_rules! timed {
@@ -12,28 +13,62 @@ macro_rules! timed {
 }
 
 fn main() {
-    const COUNT: usize = 1 << 20;
+    const COUNT: usize = 1 << 28;
 
     let mut bitslab = slab::BitmapSlab2::<usize>::with_capacity(COUNT);
 
-    let bitslab_time = timed!({
-        for i in 0..COUNT {
-            // let z = bitslab.insert(i);
-            let z = bitslab.q();
-            black_box(z);
+    dbg!(bitslab.capacity);
+
+    let mut indices_new = Vec::with_capacity(COUNT);
+    let bitslab_ins_time = timed!({
+        for i in 0..COUNT - 1 {
+            let z = bitslab.insert(i).unwrap();
+            indices_new.push(z);
         }
     });
+    println!("bitslab ins: {:?}", bitslab_ins_time);
 
-    println!("bitslab: {:?}", bitslab_time);
-
-    let mut slabmap: SlabMap<usize, usize> = SlabMap::with_capacity(COUNT);
-
-    let sm = timed!({
-        for i in 0..COUNT {
-            let q = slabmap.insert(i, i);
-            black_box(q);
+    let bitslab_rem_time = timed!({
+        for ind in indices_new {
+            let q = bitslab.remove(ind).unwrap();
         }
     });
+    println!("bitslab rem: {:?}", bitslab_rem_time);
 
-    println!("slabmap: {:?}", sm);
+    // let mut slab = Slab::<usize>::with_capacity(COUNT);
+    // let mut indices_slab = Vec::with_capacity(COUNT);
+    //
+    // let slab_ins_time = timed!({
+    //     for i in 0..COUNT {
+    //         let z = slab.insert(i);
+    //         indices_slab.push(z);
+    //     }
+    // });
+    // println!("slab ins: {:?}", slab_ins_time);
+    //
+    // let slab_rem_time = timed!({
+    //     for ind in indices_slab {
+    //         let q = slab.remove(ind);
+    //     }
+    // });
+    // println!("slab rem: {:?}", slab_rem_time);
+
+    //
+    // let mut indices_old = Vec::with_capacity(COUNT);
+    // let mut slabmap: BitmapSlab<u8> = BitmapSlab::with_capacity(COUNT);
+    // let sm_ins = timed!({
+    //     for i in 0..COUNT {
+    //         let q = slabmap.insert(i as u8);
+    //         indices_old.push(q);
+    //     }
+    // });
+    // println!("bitslab ins old: {:?}", sm_ins);
+    //
+    // let sm_rem = timed!({
+    //     for ind in indices_old {
+    //         let q = slabmap.remove(ind).unwrap();
+    //     }
+    // });
+    // println!("bitslab rem old: {:?}", sm_rem);
+
 }
--- src/slab.rs
+++ src/slab.rs
@@ -1,10 +1,15 @@
-use std::mem::MaybeUninit;
+use std::ops::Deref;
+use std::ptr::slice_from_raw_parts_mut;
+use word_bitmap;
+use word_bitmap::BitMap;
 
 type Word = u64;
 type AllocResult<T> = Result<T, AllocError>;
 const WORD_BITS: usize = Word::BITS as usize;
-const MAX_FREE_LINKS_PER_SL: usize = u8::MAX as usize - 1;
-const FLI_STEP: usize = WORD_BITS * MAX_FREE_LINKS_PER_SL;
+
+type SlotOffset = u8;
+const NULL_SLOT_OFFSET: SlotOffset = SlotOffset::MAX;
+const MAX_SLOT_OFFSET: usize = NULL_SLOT_OFFSET as usize - 1;
 
 #[derive(Debug)]
 pub enum AllocError {
@@ -12,89 +17,152 @@ pub enum AllocError {
 }
 
 struct FreeSlot {
-    next: u8,
+    next: SlotOffset,
 }
 
 impl FreeSlot {
     pub fn has_next(&self) -> bool {
-        self.next != u8::MAX
+        self.next != NULL_SLOT_OFFSET
     }
 }
 
-pub struct BitmapSlab2<T> {
-    capacity: usize,
+pub struct BitmapSlab2<T: Clone> {
+    pub(crate) capacity: usize,
+    sl_step: usize,
     fl_step: usize,
-    fl_bitmap: Word,
-    sl_bitmap: Box<[Word]>,
-    free_blocks: Box<[*mut FreeSlot]>,
-    mem: Box<[T]>,
-    mem_start_ptr: *mut Box<[T]>,
+    fl_bitmap: BitMap<Word>,
+    sl_bitmap: Box<[BitMap<Word>]>,
+    free_slots: Box<[FreeSlot]>,
+    mem_ptr: *mut T,
+    pub mem: Box<[T]>, // fixme
 }
 
-impl<T> BitmapSlab2<T> {
+impl<T: Clone> BitmapSlab2<T> {
     pub fn with_capacity(capacity: usize) -> Self {
-        let capacity_per_fl_bit = (capacity as f64 / (WORD_BITS * WORD_BITS) as f64).ceil() as usize;
-        let aligned_capacity = capacity_per_fl_bit * WORD_BITS * WORD_BITS;
+        // fixme mask fl instead of aligning with huge values
+        let sl_step = (capacity as f64 / (WORD_BITS * WORD_BITS) as f64).ceil() as usize;
+        let fl_step = sl_step * WORD_BITS;
+        let aligned_capacity = fl_step * WORD_BITS;
 
-        let mut mem = Vec::with_capacity(aligned_capacity);
-        unsafe { mem.set_len(aligned_capacity) };
-
-        let mem_start_ptr = mem.as_mut_ptr() as *mut Box<[T]>;
+        let (mem_ptr, mem, initial_free_slots) =
+            Self::initialize_mem(aligned_capacity, sl_step as u8);
 
         Self {
             capacity: aligned_capacity,
-            fl_step: capacity_per_fl_bit,
-            fl_bitmap: 0,
-            sl_bitmap: vec![0; WORD_BITS].into_boxed_slice(),
-            free_blocks: Box::new([]),
-            mem: mem.into_boxed_slice(),
-            mem_start_ptr,
+            sl_step,
+            fl_step,
+            fl_bitmap: BitMap::default(),
+            sl_bitmap: vec![BitMap::default(); WORD_BITS].into_boxed_slice(),
+            free_slots: initial_free_slots,
+            mem_ptr,
+            mem,
         }
     }
 
-    pub fn pop_free_link(&mut self, fli: usize, sli: usize) -> usize {
-        let link_head_idx = fli * FLI_STEP + sli * MAX_FREE_LINKS_PER_SL;
-        let free_slot = unsafe { &mut *self.free_blocks[link_head_idx] };
-        if !free_slot.has_next() {
-            panic!();
-        }
-        let ptr_offset = link_head_idx + free_slot.next as usize;
-        let next_ptr = unsafe {
-            self.mem_start_ptr.add(ptr_offset) as *mut FreeSlot
+    fn initialize_mem(
+        aligned_capacity: usize,
+        sl_step: SlotOffset,
+    ) -> (*mut T, Box<[T]>, Box<[FreeSlot]>) {
+        let mut initial_free_slots: Vec<FreeSlot> = Vec::with_capacity(WORD_BITS.pow(2));
+        let mem_ptr = unsafe {
+            let mut mem: Vec<T> = Vec::with_capacity(aligned_capacity);
+            mem.set_len(aligned_capacity);
+            let mem_ptr = mem.as_mut_ptr();
+            std::mem::forget(mem);
+            mem_ptr
         };
-        self.free_blocks[link_head_idx] = next_ptr;
 
-        ptr_offset
+        let mut mem_byte_ptr = mem_ptr as *mut u8;
+        for _fl_bits in 0..WORD_BITS {
+            for _sl_bits in 0..WORD_BITS {
+                Self::initialize_mem_sl(&mut mem_byte_ptr, &mut initial_free_slots, sl_step);
+            }
+        }
+
+        let mem = unsafe { Box::from_raw(slice_from_raw_parts_mut(mem_ptr, aligned_capacity)) };
+        let initial_free_slots = initial_free_slots.into_boxed_slice();
+        (mem_ptr, mem, initial_free_slots)
+    }
+
+    fn initialize_mem_sl(
+        mem_byte_ptr: &mut *mut u8,
+        initial_free_slots: &mut Vec<FreeSlot>,
+        slots_per_sl: u8,
+    ) {
+        initial_free_slots.push(FreeSlot { next: 0 });
+        for sli in 1..slots_per_sl {
+            unsafe { **mem_byte_ptr = sli };
+            *mem_byte_ptr = unsafe { mem_byte_ptr.byte_add(size_of::<T>()) };
+        }
+        unsafe { **mem_byte_ptr = NULL_SLOT_OFFSET };
+        *mem_byte_ptr = unsafe { mem_byte_ptr.byte_add(size_of::<T>()) };
+    }
+
+    fn set_bitmap_free(&mut self, fli: usize, sli: usize) {
+        let sl_word = &mut self.sl_bitmap[fli];
+        if sl_word.is_max() {
+            self.fl_bitmap.set_bit_zero(fli);
+        }
+        sl_word.set_bit_zero(sli);
+    }
+
+    fn free_slot(&mut self, index: usize) {
+        let fli = index / (WORD_BITS * WORD_BITS);
+        let sli = index % WORD_BITS;
+        let layers_offset = fli * WORD_BITS + sli;
+
+        let free_slot = &mut self.free_slots[layers_offset];
+
+        unsafe {
+            let slot_ptr = self.mem_ptr.add(index) as *mut SlotOffset;
+            *slot_ptr = free_slot.next;
+        }
+
+        free_slot.next = (index - layers_offset) as u8;
+    }
+
+    fn claim_available_slot(&mut self) -> AllocResult<usize> {
+        if self.fl_bitmap.is_max() {
+            return Err(AllocError::OutOfMemory);
+        }
+        // get available mapping mapping
+        let fli = self.fl_bitmap.first_zero_lsb() as usize;
+        let sl_word = unsafe {
+            // fli is always less than self.sl_bitmap.len()
+            self.sl_bitmap.get_unchecked_mut(fli)
+        };
+        let sli = sl_word.first_zero_lsb() as usize;
+
+        // pop head of available slots linked list
+        let free_slot = unsafe {
+            // fli will never be >= WORD_BITS, because in that case fl_bitmap is max (Err)
+            self.free_slots.get_unchecked_mut(fli * WORD_BITS + sli)
+        };
+        debug_assert!(free_slot.has_next());
+
+        let ptr_offset = fli * self.fl_step + sli * self.sl_step + free_slot.next as usize;
+        free_slot.next = unsafe { (*(self.mem_ptr.add(ptr_offset) as *const FreeSlot)).next };
+
+        // if new head is null, set bitmap to used
+        if !free_slot.has_next() {
+            sl_word.set_bit_one(sli);
+            if sl_word.is_max() {
+                self.fl_bitmap.set_bit_one(fli);
+            }
+        }
+
+        Ok(ptr_offset / size_of::<T>())
     }
 
     pub fn insert(&mut self, value: T) -> AllocResult<usize> {
-        let fli = (!self.fl_bitmap).trailing_zeros() as usize;
-        if fli == WORD_BITS {
-            return Err(AllocError::OutOfMemory);
-        }
-        let sl_word = &mut self.sl_bitmap[fli];
-        let sli = (!*sl_word).trailing_zeros();
-        let fl_offset = fli * self.fl_step;
-        let sl_byte_idx = self.sl_bitmap[fl_offset..(fl_offset + self.fl_step)]
-            .iter()
-            .position(|b| *b != Word::MAX)
-            .unwrap();
-
-        let byte_idx = fl_offset + sl_byte_idx;
-        let sl_bitmap_slot = &mut self.sl_bitmap[byte_idx];
-        let sli = (!*sl_bitmap_slot).trailing_zeros() as usize;
-        *sl_bitmap_slot |= 1 << sli;
-
-        if *sl_bitmap_slot == Word::MAX && self.sl_bitmap[fl_offset..(fl_offset + self.fl_step)]
-            .iter()
-            .rev()
-            .all(|b| *b == Word::MAX) {
-            self.fl_bitmap |= 1 << fli;
-        }
-
-        let slab_index = byte_idx * WORD_BITS + sli;
+        let slab_index = self.claim_available_slot()?;
         self.mem[slab_index] = value;
-
         Ok(slab_index)
+    }
+
+    pub fn remove(&mut self, index: usize) -> AllocResult<T> {
+        let value = self.mem[index].clone();
+        self.free_slot(index);
+        Ok(value)
     }
 }
